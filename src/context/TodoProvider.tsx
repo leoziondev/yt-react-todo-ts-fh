56267d9f439e2e2c31471a56740f47f05497edf9
LeoZion <leozionsuporte@gmail.com>
2022-03-15 16:57:05 -0300

    config initial state provider and add reducer

--- src/context/TodoProvider.tsx
+++ src/context/TodoProvider.tsx
@@ -1,14 +1,35 @@
+import { useReducer } from "react"
+import { TodoState } from "../interfaces/interfaces"
 import { TodoContext } from "./TodoContext"
+import { todoReducer } from "./todoReducer"
+
+const INITIAL_STATE: TodoState = {
+    todoCount: 2,
+    todos: [
+        {
+            id: '1',
+            desc: 'Coletar as pedras do inifinito',
+            completed: false
+        },
+        {
+            id: '2',
+            desc: 'Pedra da alma',
+            completed: false
+        },
+    ],
+    completed: 0,
+    pending: 2
+}
 
 interface Props {
     children: JSX.Element | JSX.Element[]
 }
 
-
-
 export const TodoProvider = ({ children }: Props) => {
+    const [todoState, dispatch] = useReducer(todoReducer, INITIAL_STATE)
+
   return (
-    <TodoContext.Provider value={{  }}>
+    <TodoContext.Provider value={{ todoState }}>
         {children}
     </TodoContext.Provider>
   )
