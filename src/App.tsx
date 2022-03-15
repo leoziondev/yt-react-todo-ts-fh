fa9e5aa145d300bfb57d6f538dbe9f3767420398
LeoZion <leozionsuporte@gmail.com>
2022-03-15 16:14:48 -0300

    add todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <div className="App App-header">
-      <h1>Todo</h1>
+      <Todo />
     </div>
   )
 }
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,14 @@
+const Todo = () => {
+  return (
+    <>
+        <h1>Todos</h1>
+        <ul>
+            <li>TODO 1</li>
+            <li>TODO 2</li>
+            <li>TODO 3</li>
+        </ul>
+    </>
+  )
+}
+
+export default Todo
